c826bea1b3b23321717f367ba84a8c1f52e3502f
Kevin Alavik <kevin@alavik.se>
2024-07-24 18:48:30 +0200

    feat: Added some builtin runtime funcs

--- Makefile
+++ Makefile
@@ -23,12 +23,12 @@ INCLUDE_INSTALL_DIR := /usr/local/include
 all: $(BIN_DIR)/freelibc.a
 
 $(BIN_DIR)/freelibc.a:
-	$(MAKE) -C $(COMMON_DIR) BIN_DIR=$(BIN_DIR)
+	$(MAKE) -C $(COMMON_DIR) BIN_DIR=$(BIN_DIR) INCLUDE_DIR=$(INCLUDE_DIR)
 
 install: $(BIN_DIR)/freelibc.a
 	@echo "====> Installing freelibc.a to $(LIB_DIR)"
 	install -d $(LIB_DIR)
-	install $(BIN_DIR)/freelibc.a $(LIB_DIR)/libfreelibc.a
+	install $(BIN_DIR)/obj/freelibc.a $(LIB_DIR)/libfreelibc.a
 	@echo "====> Installing headers to $(INCLUDE_INSTALL_DIR)"
 	install -d $(INCLUDE_INSTALL_DIR)
 	install -t $(INCLUDE_INSTALL_DIR) $(INCLUDE_DIR)/*
---
+++ common/builtin/__ashldi3.c
@@ -0,0 +1,37 @@
+/*
+Freelibc
+Copyright (C) 2024 Kevin Alavik and contributors
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+ * __ashldi3 - Perform an arithmetic left shift on a 64-bit integer.
+ *
+ * @param a: The long value to be shifted.
+ * @param b: The number of positions to shift `a` to the left.
+ *
+ * @return: The result of shifting `a` to the left by `b` positions.
+ */
+long __ashldi3(long a, int b)
+{
+    long result;
+    __asm__(
+        "shlq %%cl, %0"  // Shift left logical with variable shift count
+        : "=r"(result)   // Output operand: result
+        : "0"(a), "c"(b) // Input operands: a in the same register as result, b in %cl
+        : "cc"           // Clobbered registers: condition codes
+    );
+    return result;
+}
